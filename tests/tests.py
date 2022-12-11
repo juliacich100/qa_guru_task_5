949149446962b642f939fbaed291c440f65fb342
iuliiach <h46@yandex.ru>
2022-12-11 10:28:15 +0300

    added minor changes to hw 5

--- tests/tests.py
+++
@@ -1,8 +0,0 @@
-from selene.support.shared import browser
-from selene import be, have
-
-
-def test_finds_selene():
-    browser.open('/ncr').should(have.title('Google'))
-    browser.element('[name=q]').type('selene').press_enter()
-    browser.element('#search').should(have.text('User-oriented WEB UI browser'))
